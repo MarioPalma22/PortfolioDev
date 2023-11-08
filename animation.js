ab30d12585e52758f8c24c4541f039dcea627719
Mario Palma <MarioPalma2211@protonmail.com>
2023-11-07 23:33:54 -0300

    Animation fix

--- animation.js
+++ animation.js
@@ -1,22 +1,17 @@
-// $(document).ready(function () {
-//   $("a").on("click", function (event) {
-//     if (this.hash !== "") {
-      
-//       event.preventDefault();
 
-//       const hash = this.hash;
-
-//       $("html, body").animate(
-//         {
-//           scrollTop: $(hash).offset().top,
-//         },
-//         550,
-//         function () {
-//           window.location.hash = hash;
-//         }
-//       );
-//     }
-//   });
-// });
-
+function toggleCssMenu(icon) {
+    const cssmenu = document.getElementById('cssmenu');
+    if (icon.className.indexOf('active') == -1) {
+        icon.className = 'menu-icon active';
+        cssmenu.style.display = "block";
+        cssmenu.style.backgroundColor = "#230327";
+        setTimeout(function(){cssmenu.className = 'active';},0);
+        
+    }
+    else {
+        icon.className = 'menu-icon';
+        cssmenu.className = '';
+        setTimeout(function(){cssmenu.style.display = "none";},411);
+    }
+}
 
